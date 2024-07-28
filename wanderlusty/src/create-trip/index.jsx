a6beed071689a08a5a48a61df67963f75473991c
Koshik <koshik.gurbani@aceturtle.com>
2024-07-29 02:19:04 +0530

    installed react-router-dom and created one demo route and component

---
+++ wanderlusty/src/create-trip/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateTrip() {
+  return (
+    <div>
+      createTrip
+    </div>
+  )
+}
+
+export default CreateTrip
--- wanderlusty/src/main.jsx
+++ wanderlusty/src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import CreateTrip from './create-trip/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: "/create-trip",
+    element: <CreateTrip />,
+},
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
+    {/* <App /> */}
   </React.StrictMode>,
 )
